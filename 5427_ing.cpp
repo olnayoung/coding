0f8e025517faf8935213b93a1f5dd10a712d25c8
olnayoung <39485028+olnayoung@users.noreply.github.com>
2019-03-26 20:56:47 +0900

    Update 5427_ing.cpp

--- 5427_ing.cpp
+++ 5427_ing.cpp
@@ -2,13 +2,12 @@
 #include <queue>
 using namespace std;
 #define MAX 1000
+#define INF 99999999
 
 char map[MAX][MAX];
-//queue<int> sang_w, sang_h, fire_w, fire_h;
-queue<int> sang_w;
-queue<int> sang_h;
-queue<int> fire_w;
-queue<int> fire_h;
+int dx[] = { 0, 0, 1, -1 };
+int dy[] = { 1, -1, 0, 0 };
+queue<int> sang_w, sang_h, fire_w, fire_h;
 
 int hei, wid, s_w, s_h, f_w, f_h;
 int f_map[MAX][MAX], time[MAX][MAX];
@@ -21,8 +20,8 @@ void clear(std::queue<int> &q) {
 int initialize() {
 	for (int x = 0; x < hei; x++) {
 		for (int y = 0; y < wid; y++) {
-			f_map[x][y] = MAX;
-			time[x][y] = MAX;
+			f_map[x][y] = INF;
+			time[x][y] = 0;
 		}
 	}
 	return 0;
@@ -33,64 +32,43 @@ int fire_map() {
 	while (!fire_w.empty()) {
 		f_w = fire_w.front();	fire_w.pop();		f_h = fire_h.front();	fire_h.pop();
 
-		if ((map[f_h - 1][f_w] != '#') && (f_h - 1 >= 0) && (f_map[f_h - 1][f_w] > f_map[f_h][f_w])) {
-			f_map[f_h - 1][f_w] = f_map[f_h][f_w] + 1;
-			fire_h.push(f_h - 1);	fire_w.push(f_w);
-		}
-		if ((map[f_h][f_w - 1] != '#') && (f_w - 1 >= 0) && (f_map[f_h][f_w - 1] > f_map[f_h][f_w])) {
-			f_map[f_h][f_w - 1] = f_map[f_h][f_w] + 1;
-			fire_h.push(f_h);	fire_w.push(f_w - 1);
-		}
-		if ((map[f_h + 1][f_w] != '#') && (f_h + 1 < hei) && (f_map[f_h + 1][f_w] > f_map[f_h][f_w])) {
-			f_map[f_h + 1][f_w] = f_map[f_h][f_w] + 1;
-			fire_h.push(f_h + 1);	fire_w.push(f_w);
-		}
-		if ((map[f_h][f_w + 1] != '#') && (f_w + 1 < wid) && (f_map[f_h][f_w + 1] > f_map[f_h][f_w])) {
-			f_map[f_h][f_w + 1] = f_map[f_h][f_w] + 1;
-			fire_h.push(f_h);	fire_w.push(f_w + 1);
+		for (int x = 0; x < 4; x++) {
+			int new_f_h = f_h + dy[x];
+			int new_f_w = f_w + dx[x];
+
+			if ((new_f_h >= 0) && (new_f_w >= 0) && (new_f_h < hei) && (new_f_w < wid)) {
+
+				if ((map[new_f_h][new_f_w] != '#') && (f_map[new_f_h][new_f_w] > f_map[f_h][f_w] + 1)) {
+					f_map[new_f_h][new_f_w] = f_map[f_h][f_w] + 1;
+					fire_h.push(new_f_h);	fire_w.push(new_f_w);
+				}
+			}
 		}
 	}
-
-	/*for (int x = 0; x < hei; x++){
-		printf("\n");
-		for (int y = 0; y < wid; y++){
-			printf("%d ", f_map[x][y]);
-		}
-	}*/
 	return 0;
 }
 
 int BFS()
 {
 	sang_w.push(s_w);	sang_h.push(s_h);
-	time[s_h][s_w] = 1;
 
 	while (!sang_w.empty()) {
 		s_w = sang_w.front();	sang_w.pop();		s_h = sang_h.front();	sang_h.pop();
-		//printf("\n%d %d, %d %d\n", s_h, s_w, f_h, f_w);
 
 		if ((s_w == 0) || (s_w == wid - 1) || (s_h == 0) || (s_h == hei - 1)) {
-			printf("%d", time[s_h][s_w]);
+			printf("%d", time[s_h][s_w] + 1);
 			return 0;
 		}
 
-		if (map[s_h][s_w] == '*') {}
-		else {
-			if ((map[s_h - 1][s_w] == '.') && (s_h - 1 >= 0) && (time[s_h - 1][s_w] > time[s_h][s_w]) && (f_map[s_h - 1][s_w] > time[s_h][s_w])) {
-				sang_h.push(s_h - 1);	sang_w.push(s_w);
-				time[s_h - 1][s_w] = time[s_h][s_w] + 1;
-			}
-			if ((map[s_h][s_w - 1] == '.') && (s_w - 1 >= 0) && (time[s_h][s_w - 1] > time[s_h][s_w]) && (f_map[s_h][s_w - 1] > time[s_h][s_w])) {
-				sang_h.push(s_h);		sang_w.push(s_w - 1);
-				time[s_h][s_w - 1] = time[s_h][s_w] + 1;
-			}
-			if ((map[s_h + 1][s_w] == '.') && (s_h + 1 < hei) && (time[s_h + 1][s_w] > time[s_h][s_w]) && (f_map[s_h + 1][s_w] > time[s_h][s_w])) {
-				sang_h.push(s_h + 1);	sang_w.push(s_w);
-				time[s_h + 1][s_w] = time[s_h][s_w] + 1;
-			}
-			if ((map[s_h][s_w + 1] == '.') && (s_w + 1 < wid) && (time[s_h][s_w + 1] > time[s_h][s_w]) && (f_map[s_h][s_w + 1] > time[s_h][s_w])) {
-				sang_h.push(s_h);		sang_w.push(s_w + 1);
-				time[s_h][s_w + 1] = time[s_h][s_w] + 1;
+		for (int x = 0; x < 4; x++) {
+			int new_s_h = s_h + dy[x];
+			int new_s_w = s_w + dx[x];
+
+			if ((new_s_h >= 0) && (new_s_w >= 0) && (new_s_h < hei) && (new_s_w < wid)) {
+				if ((map[new_s_h][new_s_w] == '.') && (time[new_s_h][new_s_w] == 0) && (f_map[new_s_h][new_s_w] > time[s_h][s_w] + 1)) {
+					sang_h.push(new_s_h);	sang_w.push(new_s_w);
+					time[new_s_h][new_s_w] = time[s_h][s_w] + 1;
+				}
 			}
 		}
 	}
