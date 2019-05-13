66a6a9b139658899ef039b45517ca8af02e5f5c1
olnayoung <39485028+olnayoung@users.noreply.github.com>
2019-05-13 18:53:57 +0900

    Update 14889_ing.cpp

--- 14889_ing.cpp
+++ 14889_ing.cpp
@@ -4,19 +4,7 @@ using namespace std;
 int N;
 int map[20][20];
 int team_S[10], team_L[10];
-int max = 0;
-
-int print() {
-	printf("\n");
-	for (int y = 0; y < N; y++) {
-		for (int x = 0; x < N; x++) {
-			printf("%d ", map[y][x]);
-		}
-		printf("\n");
-	}
-
-	return 0;
-}
+int min = 9999999;
 
 int calcul_max() {
 	int sum1 = 0, sum2 = 0;
@@ -38,21 +26,48 @@ int calcul_max() {
 int find_max(int start, int num) {
 	int M = N / 2;
 
-	for (int x = start; x < N; x++) {
+	for (int x = start; x < N - M + num + 1; x++) {
+		team_S[num] = x;
 
+		if ((num + 1) == M) {
+			int k = 0;
+			for (int x = 0; x < N; x++) {
+				int check = 0;
+
+				for (int y = 0; y < M; y++) {
+					if (x == team_S[y]) {
+						check = 1;
+						break;
+					}
+				}
+
+				if (check == 0) {
+					team_L[k] = x;
+					k++;
+				}
+			}
+
+			min = (min > calcul_max()) ? calcul_max() : min;
+		}
+
+		if (num < M-1) {
+			find_max(x + 1, num + 1);
+		}
 	}
 	return 0;
 }
 
 int main() {
-	scanf("%d %d", &N);
+	scanf("%d", &N);
 
 	for (int y = 0; y < N; y++) {
 		for (int x = 0; x < N; x++) {
 			scanf("%d", &map[y][x]);
 		}
 	}
-	print();
+
+	find_max(0, 0);
+	printf("%d", min);
 
 	return 0;
 }
