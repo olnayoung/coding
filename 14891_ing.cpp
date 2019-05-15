82348f21b5a7a2adea92770505d20d77ece64c14
olnayoung <39485028+olnayoung@users.noreply.github.com>
2019-05-15 20:14:29 +0900

    Update 14891_ing.cpp

--- 14891_ing.cpp
+++ 14891_ing.cpp
@@ -4,6 +4,30 @@ int K;
 int n, d;
 int map[4][8];
 
+int clock(int x) {
+	int temp = map[x][7];
+
+	for (int t = 0; t < 7; t++) {
+		map[x][t+1] = map[x][t];
+	}
+
+	map[x][0] = map[x][7];
+
+	return 0;
+}
+
+int counter_clock(int x) {
+	int temp = map[x][0];
+
+	for (int t = 0; t < 7; t++) {
+		map[x][t] = map[x][t+1];
+	}
+
+	map[x][7] = map[x][0];
+
+	return 0;
+}
+
 int main() {
 	for (int y = 0; y < 4; y++) {
 		for (int x = 0; x < 8; x++) {
@@ -15,6 +39,8 @@ int main() {
 
 	for (int t = 0; t < K; t++) {
 		scanf("%d %d", &n, &d);
+
+		
 	}
 
 	return 0;
