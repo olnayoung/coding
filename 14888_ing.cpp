10d0af59047b3ac77582b07aa3a0c37d04bf47f2
olnayoung <39485028+olnayoung@users.noreply.github.com>
2019-05-18 15:57:05 +0900

    Update 14888_ing.cpp

--- 14888_ing.cpp
+++ 14888_ing.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
 
-int N, max = 0, min = 100000000;
+int N, max = -1000000000, min = 1000000000, ans;
 int number[11], oper[10];
+int already_calcul[10];
+
+int operation() {
+	ans = number[0];
+	for (int x = 0; x < N-1; x++) {
+		if (oper[already_calcul[x]] == 0) ans += number[x + 1];
+		else if (oper[already_calcul[x]] == 1) ans -= number[x + 1];
+		else if (oper[already_calcul[x]] == 2) ans = ans * number[x + 1];
+		else ans = ans / number[x + 1];
+	}
+
+	return ans;
+}
+
+int calcul(int num) {
+
+	if (num == (N-1)) {
+		int temp = operation();
+		max = (max > temp) ? max : temp;
+		min = (min < temp) ? min : temp;
+	}
+
+	for (int x = 0; x < N - 1; x++) {
+		int check = 0;
+		for (int t = 0; t < num; t++) {
+			if (already_calcul[t] == x)	{
+				check = 1;
+				break;
+			}
+		}
+		if (check == 0) {
+			already_calcul[num] = x;
+			calcul(num + 1);
+		}
+	}
+
+	return 0;
+}
 
 int main() {
 	scanf("%d", &N);
@@ -23,6 +61,10 @@ int main() {
 		}
 		temp = temp2;
 	}
+
+
+
+	calcul(0);
 
 	printf("%d\n%d", max, min);
 
