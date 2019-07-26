b2dd419ccff57bf50ce1197bf131b8b2b4660b1c
olnayoung <39485028+olnayoung@users.noreply.github.com>
2019-07-26 14:34:19 +0900

    Create 1929_ing.cpp

---
+++ 1929_ing.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#define len 1000001
+
+int A, B;
+int prime[len];
+
+int find_prime() {
+	for (int y = 2; y <= B; y++) {
+		if (prime[y] == 0){
+			for (int x = 2 * y; x <= B; x += y) {
+				prime[x] = 1;
+			}
+		}
+	}
+
+	return 0;
+}
+
+int main() {
+	scanf("%d %d", &A, &B);
+
+	find_prime();
+
+	for (int t = A; t <= B; t++) {
+		if (prime[t] == 0)	printf("%d\n", t);
+	}
+
+	return 0;
+}
