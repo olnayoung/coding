9b8fb2af4aaec18ffa82a31d55d41576db674a3f
olnayoung <39485028+olnayoung@users.noreply.github.com>
2019-07-29 17:11:49 +0900

    Create 1707_ing.cpp

---
+++ 1707_ing.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#define len 20001
+
+using namespace std;
+
+int K, V, E, a, b, check;
+vector<int> graph[len];
+queue<int>	que;
+int color[len], visit[len];
+
+int BFS(int n) {
+	que.push(n);
+
+	int a;
+	while (que.size() != 0) {
+		a = que.front();	que.pop();	visit[a] = 1;
+		for (int t = 0; t < graph[a].size(); t++) {
+			if (visit[graph[a][t]] == 0) {
+				if (color[graph[a][t]] == color[a])	return 1;
+				else {
+					que.push(graph[a][t]);
+
+					if (color[a] == 1)	color[graph[a][t]] = 2;
+					else color[graph[a][t]] = 1;
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
+int main() {
+	scanf("%d", &K);
+
+	for (int k = 0; k < K; k++) {
+		scanf("%d %d", &V, &E);
+
+		for (int e = 0; e < E; e++) {
+			scanf("%d %d", &a, &b);
+
+			graph[a].push_back(b);	graph[b].push_back(a);
+		}
+
+		check = 0;
+		for (int v = 1; v <= V; v++) {
+			if (color[v] == 0) {
+				color[v] = 1;
+				if (BFS(v) == 1) {
+					check = 1;
+					printf("NO\n");
+					break;
+				}
+			}
+		}
+
+		if (check == 0)	printf("YES\n");
+
+		for (int v = 1; v <= V; v++) {
+			graph[v].clear();
+			color[v] = 0;
+			visit[v] = 0;
+		}
+
+	}
+
+	return 0;
+}
