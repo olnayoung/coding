285668fc091a8c2073f18c57f9e7f8bc28ad3248
olnayoung <39485028+olnayoung@users.noreply.github.com>
2019-07-30 21:28:58 +0900

    Create 7576.cpp

---
+++ 7576.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <queue>
+#define len 1001
+
+using namespace std;
+
+int H, W, ans, sum;
+int map[len][len], visit[len][len];
+int dx[4] = { 1, -1, 0, 0 };
+int dy[4] = { 0, 0, 1, -1 };
+queue<int> que_y, que_x;
+
+int BFS(int h, int w) {
+	que_y.push(h);	que_x.push(w);
+
+	int y, x, new_y, new_x;
+	while (que_y.size() != 0) {
+		y = que_y.front();	que_y.pop();
+		x = que_x.front();	que_x.pop();
+
+		for (int t = 0; t < 4; t++) {
+			new_y = y + dy[t];	new_x = x + dx[t];
+
+			if ((new_y > -1) && (new_y < H) && (new_x > -1) && (new_x < W) && (map[new_y][new_x] == 0) && (visit[y][x] + 1 < visit[new_y][new_x])) {
+				que_y.push(new_y);	que_x.push(new_x);
+				visit[new_y][new_x] = visit[y][x] + 1;
+			}
+		}
+	}
+
+	return 0;
+}
+
+int main() {
+	scanf("%d %d", &W, &H);
+
+	for (int y = 0; y < H; y++) {
+		for (int x = 0; x < W; x++) {
+			scanf("%d", &map[y][x]);
+			sum += map[y][x];
+			visit[y][x] = len*100;
+		}
+	}
+
+	for (int h = 0; h < H; h++) {
+		for (int w = 0; w < W; w++) {
+			if (map[h][w] == 1)	{
+				visit[h][w] = 0;
+				BFS(h, w);
+			}
+		}
+	}
+
+	ans = 0;
+	for (int h = 0; h < H; h++) {
+		for (int w = 0; w < W; w++) {
+			if ((visit[h][w] == len * 100) && (map[h][w] == -1)) continue;
+			ans = (ans > visit[h][w]) ? ans : visit[h][w];
+		}
+	}
+
+	if (ans == len * 100)	ans = -1;
+
+	printf("%d", ans);
+
+	return 0;
+}
